bb074b8cec22b44426e95c5b77d583a2429580f9
seon-hyeong <31677745+seon-hyeong@users.noreply.github.com>
2017-09-06 11:18:40 +0900

    Add files via upload

---
+++ C_1st_assignment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+
+printf("student id number : 2017301029 \n");
+printf("name : ¹Ú¼±Çü \n");
+printf("mobile phone number : 01049024580 \n ");
+printf("GitHub address : seon-hyeong \n ");
+
+
+return 0;
+}
